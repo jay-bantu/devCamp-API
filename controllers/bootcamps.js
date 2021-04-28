b3421e145702594a227b0f7a6c54424ea03fa33b
jay <jayrgeezy@gmail.com>
2021-04-28 19:01:37 +0300

    GET for both all bootcamps and a single bootcamp

--- controllers/bootcamps.js
+++ controllers/bootcamps.js
@@ -5,15 +5,47 @@ const Bootcamp = require('../models/Bootcamps')
 // route    GET api/v1/bootcamps
 //access     public
 
-exports.getBootcamps = (req,res,next)=>{
-    res.status(200).json({success:true, msg:'show all bootcamps'});
+exports.getBootcamps =  async (req,res,next)=>{
+
+    try {
+        const bootcamps = await Bootcamp.find()
+        res.status(200).json({
+            success:true,
+            data: bootcamps
+        });
+        
+    } catch (error) {
+        res.status(400).json({
+            success: false
+        });
+        
+    }
 
 }
 //@ desc    get bootcamp
 // route    GET api/v1/bootcamps/:id
 //access     public
 
-exports.getBootcamp = (req,res,next)=>{
+exports.getBootcamp = async (req,res,next)=>{
+    try {
+        const bootcamp = await Bootcamp.findById(req.params.id)
+
+        if(!bootcamp){
+            return  res.status(400).json({success:false});
+        }
+        
+        res.status(200).json({
+            success: true,
+            data: bootcamp
+        });
+
+      
+    } catch (error) {
+        res.status(400).json({
+            success:false
+        })
+        
+    }
     res.status(200).json({success:true, msg:`get bootcamp ${req.params.id}`});
 
 }
